dd97cb70bb0d532a9b1ce3bdd0467feaacc60198
Gaurav Garg <gaurav.garg@ukg.com>
2022-07-10 09:20:21 +0530

    add

--- tollrate-service/src/main/java/com/example/tollrateservice/TollRateController.java
+++ tollrate-service/src/main/java/com/example/tollrateservice/TollRateController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RefreshScope
 @RestController
 public class TollRateController {
@@ -49,6 +55,8 @@ public class TollRateController {
 		tollratesv2.add(new TollRatev2(1003, 1.08f, Instant.now().toString(), "Yes"));
 	}
 
+	@Operation(summary = "Get all the toll rates v1")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Return toll rate - v1", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = TollRate.class))})})
 	@RequestMapping("/tollrate/{stationId}")
 	public TollRate GetTollRate(@PathVariable int stationId) {
 		System.out.println("Station requested: " + stationId);
@@ -56,6 +64,8 @@ public class TollRateController {
 		return tollrates.stream().filter(rate -> stationId == rate.getStationId()).findAny().orElse(new TollRate());
 	}
 
+	@Operation(summary = "Get all the toll rates v2")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Return toll rate - v2", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = TollRate.class))})})
 	@RequestMapping("/tollratev2/{stationId}")
 	public TollRatev2 GetTollRatev2(@PathVariable int stationId) {
 		System.out.println("Station v2 requested: " + stationId);
@@ -63,6 +73,8 @@ public class TollRateController {
 		return tollratesv2.stream().filter(rate -> stationId == rate.getStationId()).findAny().orElse(new TollRatev2());
 	}
 
+	@Operation(summary = "Get all the toll rates slow")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Return toll rate - slow", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = TollRate.class))})})
 	@RequestMapping("/tollrateslow/{stationId}")
 	public TollRate GetTollRateSlow(@PathVariable int stationId) throws InterruptedException {
 
@@ -72,6 +84,8 @@ public class TollRateController {
 		return tollrates.stream().filter(rate -> stationId == rate.getStationId()).findAny().orElse(new TollRate());
 	}
 
+	@Operation(summary = "Get all the toll rates property")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Return toll rate - property", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = TollRate.class))})})
 	@RequestMapping("/property/rate")
 	public TollRateFromProperty GetTollRateFromProperty() throws InterruptedException {
 		System.out.println("Station from property: rate: " + rateamount + ", lane: " + lanes + ", tollstart: "
